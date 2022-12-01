686c7a153ab8de7f6443bdcd4b926f8231d768e4
Aguix <77492589+Aguix@users.noreply.github.com>
2022-12-01 23:48:06 +0100

    add /informations command & fix bugs in functions

--- bot.py
+++ bot.py
@@ -16,23 +16,23 @@ discord_bot.remove_command('help')
 tree = discord_bot.tree
 
 
-@tree.error
-async def on_command_error(inte: discord.Interaction, error):
-    if isinstance(error, discord.app_commands.MissingPermissions):
-        await inte.response.send_message("Oh non, malheureux ! Tu n'as pas les permissions pour faire cette action ! Il faut être administrateur.")
-    else:
-        functions.log_error(error, inte.guild)
+# @tree.error
+# async def on_command_error(inte: discord.Interaction, error):
+#     if isinstance(error, discord.app_commands.MissingPermissions):
+#         await inte.response.send_message("Oh non, malheureux ! Tu n'as pas les permissions pour faire cette action ! Il faut être administrateur.")
+#     else:
+#         functions.log_error(error)
 
 
-@discord_bot.event
-async def on_command_error(ctx : commands.Context, error):
-    print("on_command_error")
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.reply(f"Je ne connais pas cette commande. Si tu as un trou de mémoire, la commande `/help` est là pour toi !")
-    elif isinstance(error, commands.BotMissingPermissions):
-        await ctx.reply("Je n'ai pas les permissions nécessaire à la réalisation de cette commande. Merci de verifier que j'ai bien les permissions administrateur.")
-    else:
-        functions.log_error(error, ctx.guild)
+# @discord_bot.event
+# async def on_command_error(ctx : commands.Context, error):
+#     print("on_command_error")
+#     if isinstance(error, commands.CommandNotFound):
+#         await ctx.reply(f"Je ne connais pas cette commande. Si tu as un trou de mémoire, la commande `/help` est là pour toi !")
+#     elif isinstance(error, commands.BotMissingPermissions):
+#         await ctx.reply("Je n'ai pas les permissions nécessaire à la réalisation de cette commande. Merci de verifier que j'ai bien les permissions administrateur.")
+#     else:
+#         functions.log_error(error)
 
 
 # bot.event
@@ -67,6 +67,28 @@ if True:
 
 
 
+@tree.command(name="informations", description="Découvre de nombreuses IST !")
+@discord.app_commands.describe(ist = "Le nom de l'IST pour laquelle tu veux en apprendre plus.")
+@discord.app_commands.choices( ist = [
+    discord.app_commands.Choice(name = "Chlamydiae", value = "chlamydiae")
+])
+@discord.app_commands.checks.has_permissions(administrator=True)
+async def help(inte : discord.Interaction, ist : discord.app_commands.Choice[str]):
+    ist_data = functions.get_json("ist_infos.json")[ist.value]
+    embed = discord.Embed(title = ist_data['name'], description=ist_data['description'])
+    print(ist_data)
+    for type, data in ist_data.items():
+        if data == [] or data == "":
+            continue
+        elif isinstance(data, dict):
+            value = f"・{data[0]}"
+            for el in data[1:] :
+                value += f"\n・{el}"
+        else :
+            value = data
+        embed.add_field(name = type.capitalize(), value=value)
+    await inte.response.send_message(embed = embed)
+    
 
 
 
--- functions.py
+++ functions.py
@@ -1,52 +1,14 @@
-from typing import Literal
-import discord, asyncio, numpy as np, json, datetime as dt, random, requests, re
-from PIL import Image, ImageFont, ImageDraw, ImageFilter
-from discord.ext import commands
-import setup
+import json, datetime as dt
 
 
-def get_json_file(guild_id : int, file: str) -> dict:
-    """data/{guild_id}/{file}.json"""
-    if file in setup.POSSIBLE_FILES_TYPES:
-        with open(f"data/{guild_id}/{file}.json", "r", encoding='utf-8') as f:
-            file_content : dict = json.loads(f.read())
-        return file_content
-    else:
-        print("ERROR: Problem with the type of file")
+def get_json(file: str) -> dict:
+    with open(file, "r", encoding='utf-8') as f:
+        file_content : dict = json.loads(f.read())
+    return file_content
 
+def dump_json(file: str, dump_content: dict):
+    with open(file, "w+", encoding='utf-8') as f:
+        json.dump(obj = dump_content, fp = f, indent = 4)
 
-def get_translation(language : Literal['esp', 'fra', 'usa'], command : Literal['help']) -> dict :
-    with open(f"translations/{language}.json", "r", encoding='utf-8') as f:
-        return json.loads(f.read())[command]
-
-
-def dump_json_file(guild_id: int, file: str, dump_content: dict):
-    """data/{guild_id}/{file}.json"""
-    if file in setup.POSSIBLE_FILES_TYPES:
-        with open(f"data/{guild_id}/{file}.json", "w+", encoding='utf-8') as f:
-            json.dump(obj = dump_content, fp = f, indent = 4)
-    else:
-        print("ERROR: Problem with the type of file")
-
-
-def crop_image_circle(img_path : str) : 
-    img = Image.open(img_path).convert('RGB')
-    height,width = img.size
-    lum_img = Image.new('L', [height,width] , 0)
-    draw = ImageDraw.Draw(lum_img)
-    draw.pieslice([(0,0), (height-1,width-1)], 0, 360, fill = 255, outline = None)
-    img_arr = np.asarray(img)
-    lum_img_arr = np.asarray(lum_img)
-    final_img_arr = np.dstack((img_arr,lum_img_arr))
-    res : Image = Image.fromarray(final_img_arr)
-    return res
-
-
-def log_error(error, guild : discord.Guild = None):
-    if guild == None:
-        log = f"[{dt.datetime.now()}] -- {error}\n"
-    else:
-        log = f"[{dt.datetime.now()}] - [{guild.name}] - [{guild.id}] -- {error}\n"
-    with open("bot.log", "a+") as file:
-        file.write(log)
-    print(f"\x1b[31m{log}\x1b[0m")
+def log_error(error):
+    print(f"\x1b[31m[{dt.datetime.now()}] -- {error}\n\x1b[0m")
