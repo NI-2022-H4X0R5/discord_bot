29da956ca923a22dbd51cb0d30b7fa60f1ff2bdf
Aguix <77492589+Aguix@users.noreply.github.com>
2022-12-02 06:56:48 +0100

    final commit

--- bot.py
+++ bot.py
@@ -1,74 +1,45 @@
-import discord, datetime as dt, os, requests, json
-from typing import Literal
+import discord, requests
+from datetime import datetime as dt
 from discord.ext import commands
-import config, functions
+import config
 
-if os.name == 'nt':
-    from ctypes import windll
-    k = windll.kernel32
-    k.SetConsoleMode(k.GetStdHandle(-11), 7)
-
-intents : discord.Intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-
-discord_bot = commands.Bot(command_prefix = config.PREFIX, intents = intents)
+discord_bot = commands.Bot(command_prefix = config.PREFIX, intents = discord.Intents.default())
 discord_bot.remove_command('help')
-tree = discord_bot.tree
 
 
-# @tree.error
-# async def on_command_error(inte: discord.Interaction, error):
-#     if isinstance(error, discord.app_commands.MissingPermissions):
-#         await inte.response.send_message("Oh non, malheureux ! Tu n'as pas les permissions pour faire cette action ! Il faut être administrateur.")
-#     else:
-#         functions.log_error(error)
+def log_error(error):
+    print(f"\x1b[31m[{dt.now()}] -- {error}\n\x1b[0m")
 
 
-# @discord_bot.event
-# async def on_command_error(ctx : commands.Context, error):
-#     print("on_command_error")
-#     if isinstance(error, commands.CommandNotFound):
-#         await ctx.reply(f"Je ne connais pas cette commande. Si tu as un trou de mémoire, la commande `/help` est là pour toi !")
-#     elif isinstance(error, commands.BotMissingPermissions):
-#         await ctx.reply("Je n'ai pas les permissions nécessaire à la réalisation de cette commande. Merci de verifier que j'ai bien les permissions administrateur.")
-#     else:
-#         functions.log_error(error)
+@discord_bot.tree.error
+async def on_command_error(inte: discord.Interaction, error):
+    if isinstance(error, discord.app_commands.MissingPermissions):
+        await inte.response.send_message("Oh non, malheureux ! Tu n'as pas les permissions pour faire cette action ! Il faut être administrateur.")
+    else:
+        log_error(error)
+
+@discord_bot.event
+async def on_command_error(ctx : commands.Context, error):
+    print("on_command_error")
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.reply(f"Je ne connais pas cette commande. Si tu as un trou de mémoire, la commande `/help` est là pour toi !")
+    elif isinstance(error, commands.BotMissingPermissions):
+        await ctx.reply("Je n'ai pas les permissions nécessaire à la réalisation de cette commande. Merci de verifier que j'ai bien les permissions administrateur.")
+    else:
+        log_error(error)
 
 
 # bot.event
 if True:
     @discord_bot.event
     async def on_ready():
-        await tree.sync()
-        await tree.sync(guild = discord.Object(config.SUPREM_GUILD_ID))
-        await discord_bot.change_presence(status=discord.Status.online, activity=discord.Game(f"faut_trouver_un_nom.fr"))
-        print(f"\x1b[32m{dt.datetime.now()} -- Bot Nuit de l'info OK\x1b[0m")
-
-
-    @discord_bot.event
-    async def on_guild_join(guild : discord.Guild):
-        channel : discord.TextChannel = guild.public_updates_channel if guild.public_updates_channel != None else guild.text_channels[0]
-        await channel.send(f"Merci de m'avoir ajouté au serveur ! Avec moi tout est possible, ou presque. La commande `/help` regorge de choses intéressantes !\n")
-        print(f"{dt.datetime.now()} -- Guild join : {guild.name} / {guild.member_count} membres")
-
-
-    @discord_bot.event
-    async def on_guild_remove(guild : discord.Guild):
-        suprem_channel : discord.TextChannel = discord_bot.get_channel(config.SUPREM_CHANNEL_ID)
-        await suprem_channel.send(content = f"LEFT [{guild.name}] - [{guild.id}] - [{guild.member_count} membres]")
-
-
-    @discord_bot.event
-    async def on_message(message : discord.Message):
-        if (message.content == "" and message.attachments == []) or message.author.bot == True: #c'est pour les messages system et trucs du genre
-            return
-        await discord_bot.process_commands(message)
+        await discord_bot.tree.sync()
+        await discord_bot.change_presence(status=discord.Status.online, activity=discord.Game(f""))
+        print(f"\x1b[32m{dt.now()} -- Bot EscapIST OK !\x1b[0m")
 
 
 
-
-@tree.command(name="informations", description="Découvre de nombreuses IST !")
+@discord_bot.tree.command(name="informations", description="Découvre de nombreuses IST !")
 @discord.app_commands.describe(ist = "Le nom de l'IST pour laquelle tu veux en apprendre plus.")
 @discord.app_commands.choices( ist = [
     discord.app_commands.Choice(name = "Chlamydiae", value = "0"),
@@ -78,10 +49,10 @@ if True:
 ])
 @discord.app_commands.checks.has_permissions(administrator=True)
 async def informations(inte : discord.Interaction, ist : discord.app_commands.Choice[str]):
-    ist_data = functions.get_json("ist_infos.json")[int(ist.value)]
-    # ist_data = requests.get(url="http://194.9.172.252:10000/api/ist").json()[int(ist.value)]
+    ist_data = requests.get(url="http://194.9.172.252:10000/api/ist").json()[int(ist.value)]
     embed = discord.Embed(title = ist_data['name'], description=ist_data['description'])
 
+    # j'ai honte c'est degeulasse comme code mais bon pas grave
     if 'transmission' in ist_data.keys() and ist_data['transmission'] != None:
         embed.add_field(name = "Transmission", value = "・" + '\n・'.join(ist_data['transmission']))
     if 'symptoms' in ist_data.keys() and ist_data['symptoms'] != None:
@@ -100,18 +71,14 @@ async def informations(inte : discord.Interaction, ist : discord.app_commands.Ch
     await inte.response.send_message(embed = embed)
 
 
-
-
-
-
-@tree.command(name="add_question", description="Ajouter une question dans le quizz.")
+@discord_bot.tree.command(name="add_question", description="Ajouter une question dans le quizz.")
 @discord.app_commands.describe(question = "La question à ajouter")
 @discord.app_commands.checks.has_permissions(administrator=True)
-async def add_question(inte : discord.Interaction, question : str, bonnereponse : str, reponse2 : str, reponse3 : str):
+async def add_question(inte : discord.Interaction, question : str, question_name : str, bonnereponse : str, reponse2 : str, reponse3 : str):
     url = "http://194.9.172.252:10000/api/questions"
     post_data = {
         "question" : {
-            "name": "test",
+            "name": question_name,
             "type": "question",
             "ageRange": "adulte",
             "content": question,
@@ -133,14 +100,11 @@ async def add_question(inte : discord.Interaction, question : str, bonnereponse 
         }
     ]
     }
-    requests.post(url, json = post_data)
-
-
-
-    await inte.response.send_message("La question a été ajoutée avec succès !")
-
-
-
+    req = requests.post(url, json = post_data)
+    if req.status_code != 200 or req != 201:
+        await inte.response.send_message("Ce nom de question est déjà utilisé !")
+    else:
+        await inte.response.send_message("La question a été ajoutée avec succès !")
 
 
 discord_bot.run(config.TOKEN)
